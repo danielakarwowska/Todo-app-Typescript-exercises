4c4ee8a7831dce792604e3a64b4784cbf1127e41
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-03-08 11:39:07 +0100

    html added styles.css

--- dist/app.js
+++ dist/app.js
@@ -2,3 +2,5 @@
 // <label for="task-1">Zadania</label>
 // <input type="checkbox" id="task-1" name="Zadania"/>
 // </li>
+const add = (v1, v2) => v1 + v2;
+console.log(add(10, 50));
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+// <li>
+// <label for="task-1">Zadania</label>
+// <input type="checkbox" id="task-1" name="Zadania"/>
+// </li>
+const add =( v1: number, v2: number) => v1 + v2;
+
+console.log(add(10,50));
