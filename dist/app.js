a994b317d65a7ad3b87d482b530e8529b0cc4630
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-03-08 13:38:52 +0100

    add task to list

--- dist/app.js
+++ dist/app.js
@@ -2,5 +2,24 @@
 // <label for="task-1">Zadania</label>
 // <input type="checkbox" id="task-1" name="Zadania"/>
 // </li>
-const add = (v1, v2) => v1 + v2;
-console.log(add(10, 50));
+const taskNameInputElement = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
+const taskContainerElement = document.querySelector(".tasks");
+const tasks = [""];
+const render = () => {
+    taskContainerElement.innerHTML = "";
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task;
+        taskContainerElement.appendChild(taskElement);
+    });
+};
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    tasks.push(taskNameInputElement.value);
+    render();
+});
+render();
