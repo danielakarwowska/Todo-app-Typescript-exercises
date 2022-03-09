08856c66852f278bff4b43b4eaf92441acd90d3f
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-03-09 10:16:36 +0100

    object - added tasks

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,60 @@
-// <li>
-// <label for="task-1">Zadania</label>
-// <input type="checkbox" id="task-1" name="Zadania"/>
-// </li>
 const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
 const addButtonElement: HTMLButtonElement = document.querySelector("button");
-const taskContainerElement: HTMLElement = document.querySelector(".tasks");
+const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
 
-const tasks: string[]=[""];
-
-const render = () => {
-    taskContainerElement.innerHTML = "";
-    tasks.forEach((task) => {
-        const taskElement: HTMLElement = document.createElement("li");
-        taskElement.innerText = task;
-        taskContainerElement.appendChild(taskElement);
-    });
-};
-const addTask = (task: string)=>{
-    tasks.push(task);
+interface Task{
+    title: string;
+    done: boolean;
 }
 
-addButtonElement.addEventListener("click",(event: Event) => {
-    event.preventDefault();
-    tasks.push(taskNameInputElement.value) ;
-    render();
+const tasks: Task []= [
+  {
+    title: "isc na silownie",
+    done: false,
+  },
+  {
+    title: "isc na silownie",
+    done: false,
+  },
+  {
+    title: "isc na silownie",
+    done: false,
+  },
+];
+
+const render = () => {
+  tasksContainerElement.innerHTML = "";
+  tasks.forEach((task,index) => {
+    const taskElement: HTMLElement = document.createElement("li");
+    const id: string= 'task-${index}';
+    
+    const labelElement: HTMLLabelElement = document.createElement("label");
+    labelElement.innerText = task.title;
+    labelElement.setAttribute("for", id);
+
+    const checkboxElement: HTMLInputElement = document.createElement("input");
+    checkboxElement.type = "checkbox";
+    checkboxElement.name = task.title;
+    checkboxElement.id = id;
+    checkboxElement.checked = task.done;
+    checkboxElement.addEventListener("change",() =>{
+        task.done = !task.done;
+    });
+
+    taskElement.appendChild(labelElement);
+    taskElement.appendChild(checkboxElement);
+
+    tasksContainerElement.appendChild(taskElement);
+  });
+};
+const addTask = (task: Task) => {
+  tasks.push(task);
+};
+
+addButtonElement.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+  addTask({title: taskNameInputElement.value, done: false});
+  render();
 });
 
 render();
